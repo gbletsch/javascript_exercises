8ed89e0ef3f53478a3d0d207b8ecaa583839d0bf
Guilherme <gbletsch@gmail.com>
2019-03-28 19:48:00 -0300

    A fully curved spaceship

--- drawing.js
+++ drawing.js
@@ -50,7 +50,8 @@ function pacman(ctx, x, y, radius, open) {
 function draw_ship(ctx, radius, options) {
   options = options || {};
   let angle = (options.angle || .5 * Math.PI) / 2;
-  let curve = options.curve || .5;
+  let curve1 = options.curve1 || .25;
+  let curve2 = options.curve2 || .75;
   ctx.save();
   if(options.guide) {
     ctx.strokeStyle = "white";
@@ -66,27 +67,50 @@ function draw_ship(ctx, radius, options) {
   ctx.fillStyle = options.fill || "black";
   ctx.beginPath();
   ctx.moveTo(radius, 0);
-  ctx.lineTo(
+  ctx.quadraticCurveTo(
+    Math.cos(angle) * radius * curve2,
+    Math.sin(angle) * radius * curve2,
     Math.cos(Math.PI - angle) * radius,
     Math.sin(Math.PI - angle) * radius
   );
-  ctx.quadraticCurveTo(radius * curve - radius, 0,
+  ctx.quadraticCurveTo(-radius * curve1, 0,
     Math.cos(Math.PI + angle) * radius,
     Math.sin(Math.PI + angle) * radius
+  );
+  ctx.quadraticCurveTo(
+    Math.cos(-angle) * radius * curve2,
+    Math.sin(-angle) * radius * curve2,
+    radius, 0
   );
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
   if(options.guide) {
     ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
     ctx.lineWidth = .5;
     ctx.beginPath();
+    ctx.moveTo(
+      Math.cos(-angle) * radius,
+      Math.sin(-angle) * radius
+    );
+    ctx.lineTo(0, 0);
     ctx.moveTo(-radius, 0);
     ctx.lineTo(0, 0);
     ctx.stroke();
     ctx.beginPath();
-    ctx.arc(radius * curve - radius, 0, radius / 50, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.arc(
+      Math.cos(angle) * radius * curve2,
+      Math.sin(angle) * radius * curve2,
+      radius / 40, 0, 2 * Math.PI
+    );
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(
+      radius * curve1 - radius, 0,
+      radius / 50, 0,
+      2 * Math.PI
+    );
   }
   ctx.restore();
 }
