54ea1db3de8c76eb546819f5118f1e15af359c0f
Guilherme <gbletsch@gmail.com>
2019-03-29 17:41:50 -0300

    Figure 5-3. Using the same shape with increasing noise

--- drawing.js
+++ drawing.js
@@ -125,18 +125,15 @@ Math.PI);
 ctx.restore();
 }
 
-function draw_asteroid(ctx, radius, segments, options) {
+function draw_asteroid(ctx, radius, shape, options) {
   options = options || {};
   ctx.strokeStyle = options.stroke || "white";
   ctx.fillStyle = options.fill || "black";
   ctx.save();
   ctx.beginPath();
-  for(let i = 0; i < segments; i++) {
-    ctx.rotate(2 * Math.PI / segments);
-    ctx.lineTo(radius + radius * options.noise * (Math.random() - 0.5), 0);
-    // ctx.lineTo(radius * 0.8 + radius * 0.4 * Math.random(), 0);
-    // ctx.lineTo(radius * Math.random(), 0);
-    // ctx.lineTo(radius, 0);
+  for(let i = 0; i < shape.length; i++) {
+    ctx.rotate(2 * Math.PI / shape.length);
+    ctx.lineTo(radius + radius * options.noise * shape[i], 0);
   }
   ctx.closePath();
   ctx.fill();
@@ -146,6 +143,43 @@ function draw_asteroid(ctx, radius, segments, options) {
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.stroke();
+    ctx.beginPath();
+    ctx.lineWidth = 0.2;
+    ctx.arc(0, 0, radius + radius * options.noise, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius - radius * options.noise, 0, 2 * Math.PI);
+    ctx.stroke();
   }
   ctx.restore();
 }
+
+
+// function draw_asteroid(ctx, radius, shape, options) {
+//   options = options || {};
+//   ctx.strokeStyle = options.stroke || "white";
+//   ctx.fillStyle = options.fill || "black";
+//   ctx.save();
+//   ctx.beginPath();
+//   for(let i = 0; i < shape.lenght; i++) {
+//     ctx.rotate(2 * Math.PI / shape.lenght);
+//     ctx.lineTo(radius + radius * options.noise * shape[i], 0);
+//   }
+//   ctx.closePath();
+//   ctx.fill();
+//   ctx.stroke();
+//   if(options.guide) {
+//     ctx.lineWidth = 0.5;
+//     ctx.beginPath();
+//     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+//     ctx.stroke();
+//     ctx.beginPath();
+//     ctx.lineWidth = .2;
+//     ctx.arc(0, 0, radius + radius * options.noise, 0, 2 * Math.PI);
+//     ctx.stroke();
+//     ctx.beginPath();
+//     ctx.arc(0, 0, radius - radius * options.noise, 0, 2 * Math.PI);
+//     ctx.stroke();
+//   }
+//   ctx.restore();
+// }
