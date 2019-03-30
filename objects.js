e9fbb78b3444f51eae38b3995b140c41bc7ae3c1
Guilherme <gbletsch@gmail.com>
2019-03-30 15:33:07 -0300

    Figure 8-1. Pac-Man snapping and turning

--- objects.js
+++ objects.js
@@ -41,3 +41,69 @@ Asteroid.prototype.draw = function(ctx, guide){
   });
   ctx.restore();
 }
+
+function PacMan(x, y, radius, speed){
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.speed = speed;
+  this.angle = 0;
+  this.x_speed = speed;
+  this.y_speed = 0;
+  this.time = 0;
+  this.mouth = 0;
+}
+
+PacMan.prototype.draw = function(ctx){
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.rotate(this.angle);
+  draw_pacman(ctx, this.radius, this.mouth);
+  ctx.restore();
+}
+
+PacMan.prototype.turn = function(direction){
+  if (this.y_speed){
+    this.x_speed = -direction * this.y_speed;
+    this.y_speed = 0;
+    this.angle = this.x_speed > 0 ? 0 : Math.PI;
+  } else {
+    this.y_speed = direction * this.x_speed;
+    this.x_speed = 0;
+    this.angle = this.y_speed > 0 ? 0 : Math.PI;
+  }
+}
+
+PacMan.prototype.turn_left = function(){
+  this.turn(-1);
+}
+PacMan.prototype.turn_right = function(){
+  this.turn(1);
+}
+
+PacMan.prototype.update = function(elapsed, width, height){
+  // an average of once per 100 frames
+  if(Math.random() <= .01){
+    if(Math.random < .5){
+      this.turn_left();
+    } else {
+      this.turn_right();
+    }
+  }
+  if(this.x - this.radius + elapsed * this.x_speed > width) {
+    this.x = -this.radius;
+  }
+  if(this.x + this.radius + elapsed * this.x_speed < 0) {
+    this.x = width + this.radius;
+  }
+  if(this.y - this.radius + elapsed * this.y_speed > height) {
+    this.y = -this.radius;
+  }
+  if(this.y + this.radius + elapsed * this.y_speed < 0) {
+    this.y = height + this.radius;
+  }
+  this.x += this.x_speed * elapsed;
+  this.y += this.y_speed * elapsed;
+  this.time += elapsed;
+  this.mouth = Math.abs(Math.sin(2 * Math.PI * this.time));
+}
