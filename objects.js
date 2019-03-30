23e731e9a2833cf3c67e144dba943b87656f1561
Guilherme <gbletsch@gmail.com>
2019-03-30 14:19:07 -0300

    Figure 7-2. Three asteroids floating about

--- objects.js
+++ objects.js
@@ -12,3 +12,32 @@ function Asteroid(segments, radius, noise){
     this.shape.push(Math.random() - .5);
   }
 }
+
+Asteroid.prototype.update = function(elapsed){
+  if(this.x - this.radius + elapsed * this.x_speed > canvas.width){
+    this.x = -this.radius;
+  }
+  if(this.x + this.radius + elapsed * this.x_speed < 0){
+    this.x = canvas.width + this.radius;
+  }
+  if(this.y - this.radius + elapsed * this.y_speed > canvas.height){
+    this.y = -this.radius;
+  }
+  if(this.y + this.radius + elapsed * this.y_speed < 0){
+    this.y = canvas.height + this.radius;
+  }
+  this.x += elapsed * this.x_speed;
+  this.y += elapsed * this.y_speed;
+  this.angle = (this.angle + this.rotation_speed * elapsed) % (2 * Math.PI);
+}
+
+Asteroid.prototype.draw = function(ctx, guide){
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.rotate(this.angle);
+  draw_asteroid(ctx, this.radius, this.shape, {
+    guide: this.guide,
+    noise: this.noise
+  });
+  ctx.restore();
+}
